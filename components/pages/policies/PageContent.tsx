2f176999259142f4ddefce48760a97c0337dc664
Anders Richardsson <2107110+caravinci@users.noreply.github.com>
2021-04-26 12:23:23 -0400

    update policies content

--- components/pages/policies/PageContent.tsx
+++ components/pages/policies/PageContent.tsx
@@ -1,9 +1,11 @@
-import { css } from '@emotion/core';
 import { ReactElement } from 'react';
+import { css } from '@emotion/core';
 import { useTheme } from 'emotion-theming';
 
 import defaultTheme from '../../theme';
-import StyledLink from '../../Link';
+import Guidelines from './Guidelines';
+import Privacy from './Privacy';
+import Usage from './Usage';
 
 const PageContent = (): ReactElement => {
   const theme: typeof defaultTheme = useTheme();
@@ -20,134 +22,51 @@ const PageContent = (): ReactElement => {
       <article
         css={css`
           box-sizing: border-box;
-          display: flex;
-          flex-wrap: wrap;
           margin: 30px 0;
           max-width: 870px;
           padding: 40px;
           width: 100%;
           ${theme.shadow.default};
 
-          @media (min-width: 900px) {
+          .bold {
+            font-weight: bold;
           }
-        `}
-      >
-        <h1
-          css={css`
+
+          h1 {
             color: ${theme.colors.primary};
             font-size: 26px;
             font-weight: normal;
             margin: 0;
-          `}
-        >
-          Website and Data Usage Policies
-        </h1>
+          }
 
-        <section>
-          <h2
-            css={css`
-              ${theme.typography.subheading};
-            `}
-          >
-            General
-          </h2>
-          <p>
-            The Canadian VirusSeq Data Portal promotes data sharing that provides the research
-            community access and use to the data and analysis according to{' '}
-            <StyledLink
-              href="https://www.go-fair.org/fair-principles/"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              FAIR data principles
-            </StyledLink>
-            .
-          </p>
-          <p>
-            The Canadian VirusSeq Data Portal is not providing access to any personal data at the
-            current time. If you are submitting sequences or metadata to the Canadian VirusSeq Data
-            Portal, do not include any data that could reveal the personal identity of the source.
-          </p>
-        </section>
+          h2 {
+            ${theme.typography.subheading};
+          }
 
-        <section>
-          <h2
-            css={css`
-              ${theme.typography.subheading};
-            `}
-          >
-            Disclaimer
-          </h2>
-          <p>
-            COVID-19 is an emerging, rapidly evolving situation. To promote responsive,
-            collaborative, research and public health surveillance, virus sequences and minimal
-            metadata are released on the Canadian VirusSeq Data Portal rapidly and should be
-            considered draft and subject to change.
-          </p>
-          <p>
-            Beyond limited editorial control and some internal integrity checks, the quality and
-            accuracy of the record are the responsibility of the submitting author, not of the data
-            portal. It is also the responsibility of the submitters to ascertain that they have the
-            right to submit the data. The VirusSeq team will work with the submitters to provide
-            feedback on metadata and sequence data to improve the overall quality and consistency of
-            the data submitted.
-          </p>
-        </section>
+          ol,
+          ul {
+            padding-left: 30px;
 
-        <section>
-          <h2
-            css={css`
-              ${theme.typography.subheading};
-            `}
-          >
-            Recognition of the work of the data producer
-          </h2>
-          <p>
-            You may use the data from the Canadian VirusSeq Data Portal to author results obtained
-            from your analyses of relevant data, provided that your published results acknowledge,
-            as the original source of the data, CPLHN, CanCOGeN-VirusSeq, the laboratory where the
-            clinical specimen(s) and/or virus isolate(s) were first obtained and if applicable, the
-            laboratory where the data have been generated from the isolate(s) and/or specimen(s)
-            received and submitted to the Canadian VirusSeq Data Portal. Laboratories should be
-            identified by geographic location and address.
-          </p>
-          <p>
-            Please note that the data that is being shared is the work of many individuals and
-            should be treated as unpublished data. If you wish to publish research using the data,
-            <StyledLink
-              href="https://www.genomecanada.ca/en/about/contact-us"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              contact us
-            </StyledLink>{' '}
-            first to ensure that those who have generated the data can be involved in its analysis.
-            You are responsible to make best efforts to collaborate with representatives of the
-            Originating Laboratory responsible for obtaining the specimen(s) and involve them in
-            such analyses and further research using such Data.
-          </p>
-        </section>
+            li {
+              &:not(:last-of-type) {
+                margin-bottom: 5px;
+              }
 
-        <section>
-          <h2
-            css={css`
-              ${theme.typography.subheading};
-            `}
-          >
-            Intellectual Property
-          </h2>
-          <p>
-            The Canadian VirusSeq Data Portal is designed to provide and encourage access within the
-            scientific community to the most up-to-date and comprehensive COVID virus sequence
-            information. Therefore, there are no restrictions on the use or distribution of the
-            Canadian VirusSeq Data Portal data. While we do not encourage this practice, some
-            submitters may claim patent, copyright, or other intellectual property rights in all or
-            a portion of the data they have submitted. The Canadian VirusSeq data portal is not in a
-            position to assess the validity of such claims, and therefore cannot provide comment or
-            unrestricted permission concerning the use, copying, or distribution of the information
-            it contains. Source NCBI modified.
-          </p>
-        </section>
+              ol,
+              ul {
+                margin-top: 5px;
+              }
+            }
+          }
+
+          section:not(:first-of-type) {
+            margin-top: 40px;
+          }
+        `}
+      >
+        <Usage />
+        <Privacy />
+        <Guidelines />
       </article>
     </main>
   );
---
+++ components/pages/policies/Usage.tsx
@@ -0,0 +1,164 @@
+import { ReactElement } from 'react';
+
+import StyledLink from '../../Link';
+
+const Usage = (): ReactElement => (
+  <section>
+    <h1>Website and Data Usage Policies</h1>
+
+    <h2>General</h2>
+
+    <p>
+      The Canadian VirusSeq Data Portal (CVDP, also referred to as "the Portal") is an open-access
+      data portal intended to facilitate access to Canadian SARS-CoV-2 sequences and associated
+      non-sensitive metadata adhering to the FAIR Data Principles{' '}
+      <StyledLink
+        href="https://www.go-fair.org/fair-principles/"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        FAIR data principles
+      </StyledLink>
+      . The Portal will manage and share limited contextual metadata and viral genome sequences
+      among Canadian public health labs, researchers and other groups interested in accessing the
+      data for surveillance, research, and innovation purposes. In doing so, it will complement the
+      controlled-access platform developed by the National Microbiology Laboratory (NML).
+    </p>
+
+    <p>
+      The Portal will harmonize, validate, and automate submission to international databases and
+      enable the creation of real-time dashboards that summarize the Canadian data contributions
+      while facilitating exploration and access. The Data Portal is funded by Genome Canada.
+    </p>
+
+    <p>
+      The CVDP is not providing access to any personal data at the current time. If you are
+      submitting sequences or metadata to the CVDP, do not include any data that could reveal the
+      personal identity of the source.
+    </p>
+
+    <h2>Disclaimer</h2>
+
+    <p>
+      COVID-19 is an emerging and rapidly evolving situation. To promote responsive, collaborative,
+      research and public health surveillance, virus sequences and minimal metadata are released on
+      the CVDP rapidly and should be considered draft and subject to change.
+    </p>
+
+    <p>
+      Beyond limited editorial and quality controls and some internal integrity checks, the quality
+      and accuracy of the record are the responsibility of submitters, not of the CVDP. It is also
+      the responsibility of submitters to ascertain that they have the right to submit the data. The
+      CVDP team will work with submitters to provide feedback on metadata and sequence data to
+      improve the overall quality and consistency of the data submitted.
+    </p>
+
+    <h2>Data available</h2>
+
+    <p>
+      Currently, the CVDP exclusively focuses on providing access to data types that do not
+      constitute "personal data/information". These include:
+    </p>
+
+    <ul>
+      <li>Consensus viral sequence</li>
+      <li>Raw de-hosted viral sequences</li>
+      <li>
+        Contextual Metadata:
+        <ol>
+          <li>Sample name — a unique identifier for each sequenced specimen</li>
+          <li>
+            Sample collected by — the name of the health authority that collected the original
+            sample
+          </li>
+          <li>
+            Sequence submitted by — the name of the health authority that generated the sequence
+          </li>
+          <li>Sample collection date — the date on which the sample was collected</li>
+          <li>Country of origin — the country where the sample was collected.</li>
+          <li>
+            Province/Territory of origin — the province or territory where the sample was collected
+            from
+          </li>
+          <li>Organism — the taxonomic name of the organism.</li>
+          <li>Isolate — the identifier for the specific isolate (if an isolate was sequenced)</li>
+          <li>Isolation source — the material sampled</li>
+          <li>Host — the taxonomic name of the host</li>
+          <li>Host disease — the name of the disease experienced by the host</li>
+          <li>Host age — the age of the host at the time of sampling</li>
+          <li>Host gender — the gender of the host at the time of sample collection</li>
+          <li>Sequencing instrument — the make and model of the sequencing instrument used</li>
+          <li>
+            Assembly method — the name software method used to generate the consensus sequence.
+          </li>
+        </ol>
+      </li>
+    </ul>
+
+    <h2>Data users guidelines</h2>
+
+    <p>
+      Access to the data provided within the CVDP is provided in a completely open manner, and at no
+      cost to members of the scientific community and other interested parties. Nevertheless, users
+      are expected to follow the CVDP policy on Recognition of the work of data submitters. Users
+      should not attempt to make use of the portal data to attempt to re-identify specific
+      individuals. In the unlikely case you come across identifying data, please swiftly report the
+      event, indicating the problematic dataset, at{' '}
+      <StyledLink
+        href="mailto:info@virusseq-dataportal.ca"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        info@virusseq-dataportal.ca
+      </StyledLink>
+      .
+    </p>
+
+    <h2>Recognition of the work of the data submitters</h2>
+
+    <p>
+      You may use the data from the CVDP to author results obtained from your analyses of relevant
+      data, provided that{' '}
+      <span className="bold">
+        your published results acknowledge, as the original source of the data, CanCOGeN-VirusSeq,
+        CPLHN and its members
+      </span>
+      .
+    </p>
+
+    <p>
+      Proposed sentence: “The authors of the manuscript would like to acknowledge the original
+      source of the data CanCOGeN-VirusSeq, CPLHN and its members.”
+    </p>
+
+    <p>
+      Please note that the data that is being shared is the work of many individuals and should be
+      treated as unpublished data. If you wish to publish research using the data, contact us at{' '}
+      <StyledLink
+        href="mailto:info@virusseq-dataportal.ca"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        info@virusseq-dataportal.ca
+      </StyledLink>{' '}
+      first to ensure that those who have generated the data can be involved in its analysis. You
+      are responsible for making the best efforts to collaborate with representatives of the
+      Originating Laboratory responsible for obtaining the specimen(s) and involve them in such
+      analyses and further research using such Data.
+    </p>
+
+    <h2>Intellectual Property</h2>
+
+    <p>
+      The CVDP is designed to provide and encourage access within the scientific community to the
+      most up-to-date and comprehensive COVID-19 viral sequencing data. Therefore, there are no
+      restrictions on the use or distribution of the CVDP data. While we do not encourage this
+      practice, some submitters may claim patent, copyright, or other intellectual property rights
+      in all or a portion of the data they have submitted. The CVDP is not in a position to assess
+      the validity of such claims and therefore cannot provide comment or unrestricted permission
+      concerning the use, copying, or distribution of the information it contains.
+    </p>
+  </section>
+);
+
+export default Usage;
