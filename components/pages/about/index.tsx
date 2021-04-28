c19fdc568ab117056da8c34ccf2af4435ce094c7
Anders Richardsson <2107110+caravinci@users.noreply.github.com>
2021-04-28 12:40:25 -0400

    improve seo by changing landing page title

--- components/pages/about/index.tsx
+++ components/pages/about/index.tsx
@@ -4,7 +4,7 @@ import PageContent from './PageContent';
 import PageLayout from '../../PageLayout';
 
 const AboutPage = (): ReactElement => (
-  <PageLayout subtitle="About">
+  <PageLayout>
     <PageContent />
   </PageLayout>
 );
