0d04aa3a83f165058974dec6414ffe61f53ea910
jaserud <udjaser@gmail.com>
2021-05-31 12:04:11 -0400

    use exisiting components for links

--- components/pages/explorer/DownloadInfoModal.tsx
+++ components/pages/explorer/DownloadInfoModal.tsx
@@ -1,5 +1,5 @@
 import { css } from '@emotion/react';
-import React from 'react';
+import StyledLink, { InternalLink } from '../../Link';
 import { Modal } from '../../Modal';
 import defaultTheme from '../../theme';
 
@@ -15,15 +15,21 @@ const DownloadInfoModal = ({ onClose }: { onClose: () => void }) => {
       >
         <p>
           Your download has started. By downloading this data, you agree to{' '}
-          <a href="./acknoledgements">acknowledge</a> the Canadian Public Health Laboratory Network
-          (CPHLN), CanCOGeN VirusSeq, all laboratories having contributed data and follow all{' '}
-          <a href="./policies">CVDP policies</a>.
+          <InternalLink path="/acknoledgements">acknowledge</InternalLink> the Canadian Public
+          Health Laboratory Network (CPHLN), CanCOGeN VirusSeq, all laboratories having contributed
+          data and follow all <InternalLink path="./policies">CVDP policies</InternalLink>.
         </p>
         <p>
           Data that is being shared is the work of many individuals and should be treated as
-          unpublished data. If you wish to publish research using the data, contact us at
-          <a href="mailto:info@virusseq-dataportal.ca">info@virusseq-dataportal.ca</a> first to
-          ensure that those who have generated the data can be involved in its analysis.
+          unpublished data. If you wish to publish research using the data, contact us at{' '}
+          <StyledLink
+            href="mailto:info@virusseq-dataportal.ca"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            info@virusseq-dataportal.ca
+          </StyledLink>{' '}
+          first to ensure that those who have generated the data can be involved in its analysis.
         </p>
       </p>
     </Modal>
