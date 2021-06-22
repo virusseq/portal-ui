29e4b5b19f789d30c3561b8cd9fc04abfa22e8e1
Alex Lepsa <lepsalex@gmail.com>
2021-06-22 11:09:56 -0400

    structure and linking based on team page

---
+++ components/pages/acknowledgements/Partners.tsx
@@ -0,0 +1,63 @@
+/*
+ *
+ * Copyright (c) 2021 The Ontario Institute for Cancer Research. All rights reserved
+ *
+ *  This program and the accompanying materials are made available under the terms of
+ *  the GNU Affero General Public License v3.0. You should have received a copy of the
+ *  GNU Affero General Public License along with this program.
+ *   If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ *  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ *  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ *  SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ *  TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ *  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ *  IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+import { ReactElement } from 'react';
+import { css, useTheme } from '@emotion/react';
+
+import StyledLink from '../../Link';
+import defaultTheme from '../../theme';
+
+const Partners = (): ReactElement => {
+  const theme: typeof defaultTheme = useTheme();
+  return (
+    <section
+      css={css`
+        margin: 5px 0 10px;
+      `}
+    >
+      <h2
+        css={css`
+          ${theme.typography.subheading};
+        `}
+      >
+        Technology Partners
+      </h2>
+
+      <p>
+        The development and implementation of the Canadian VirusSeq Data Portal is led by{' '}
+        <StyledLink
+          href="https://www.computationalgenomics.ca/c3g-montreal-node/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Dr. Guillaume Bourque (McGill University) and his team
+        </StyledLink>
+        . This team, in collaboration with CanCOGeN and world-leading genomics scientists
+        specializing in data science and policy, including Drs. Fiona Brinkman (Simon Fraser
+        University), William Hsiao (Simon Fraser University), Lincoln Stein and Christina Yung
+        (Ontario Institute for Cancer Research) and Yann Joly (McGill University), oversees the
+        technical interactions with the National Microbiology Laboratory and provincial public
+        health laboratories across the country.
+      </p>
+    </section>
+  );
+};
+export default Partners;
---
+++ components/pages/acknowledgements/Portal.tsx
@@ -0,0 +1,92 @@
+/*
+ *
+ * Copyright (c) 2021 The Ontario Institute for Cancer Research. All rights reserved
+ *
+ *  This program and the accompanying materials are made available under the terms of
+ *  the GNU Affero General Public License v3.0. You should have received a copy of the
+ *  GNU Affero General Public License along with this program.
+ *   If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ *  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ *  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ *  SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ *  TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ *  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ *  IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+import { ReactElement } from 'react';
+import { css } from '@emotion/react';
+
+import StyledLink from '../../Link';
+import OvertureLogoWithText from '../../theme/icons/overture_logo_with_text';
+import OicrLogo from '../../theme/icons/oicr';
+
+const Portal = (): ReactElement => (
+  <section>
+    <div
+      css={css`
+        display: flex;
+      `}
+    >
+      <p>
+        <span
+          css={css`
+            font-weight: bold;
+          `}
+        >
+          VirusSeq Data Portal:{' '}
+        </span>
+        The VirusSeq Data Portal was designed and developed by the{' '}
+        <StyledLink
+          href="https://softeng.oicr.on.ca/team/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Genome Informatics Team
+        </StyledLink>{' '}
+        at the{' '}
+        <StyledLink href="https://www.oicr.on.ca">Ontario Institute for Cancer Research</StyledLink>
+        , led by Dr. Christina Yung, Director of Genome Informatics and Dr. Lincoln Stein, Head of
+        Adaptive Oncology. The software is powered by{' '}
+        <StyledLink href="https://www.overture.bio" rel="noopener noreferrer" target="_blank">
+          Overture
+        </StyledLink>
+        , an open-source software suite for managing and sharing data at scale on compute clouds.
+      </p>
+
+      <span
+        css={css`
+          width: 190px;
+          margin: 0px 0 0 35px;
+          padding: 25px 20px;
+          border-radius: 10px;
+          border: solid 1px #dfdfe1;
+        `}
+      >
+        <StyledLink href="https://overture.bio/" rel="noopener noreferrer" target="_blank">
+          <OvertureLogoWithText
+            width={123}
+            style={css`
+              margin: -12px 0px 0px 4px;
+              object-fit: contain;
+            `}
+          />
+        </StyledLink>
+        <StyledLink href="https://oicr.on.ca" rel="noopener noreferrer" target="_blank">
+          <OicrLogo
+            style={css`
+              margin: 10px 0 0 10px;
+            `}
+          />
+        </StyledLink>
+      </span>
+    </div>
+  </section>
+);
+
+export default Portal;
