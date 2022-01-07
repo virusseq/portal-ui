4fc061c2e9f085c7eb8ac134c334ed96b339f855
Scott Cain <scott+git@scottcain.net>
2022-01-07 09:40:12 -0800

    Update Contributors.tsx
    Improved the CanCOGeN funding sentence

--- components/pages/acknowledgements/Contributors.tsx
+++ components/pages/acknowledgements/Contributors.tsx
@@ -74,7 +74,7 @@ const Contributors = (): ReactElement => {
         </p>
       </LoaderWrapper>
 
-    <p>Funding for the VirusSeq website was provided by Genome Canada via a CANCoGEN grant.</p>
+    <p>Funding for the VirusSeq Data Portal is provided by The Canadian COVID Genomics Network (CanCOGeN), and supported by Genome Canada and Innovation, Science and Economic Development Canada (ISED)</p>
 
     </section>
   );
