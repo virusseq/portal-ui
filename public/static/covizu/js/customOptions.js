7867167ad3a2dc6bc2380f7ea41274ee17ef52c2
Sam Rich <samrichca@users.noreply.github.com>
2022-01-11 16:35:49 -0500

    #164: covizu: update version handling, revisions (#168)

--- public/static/covizu/js/customOptions.js
+++ public/static/covizu/js/customOptions.js
@@ -1,45 +1,9 @@
 // get data info from current URL
 var urlParams = new URLSearchParams(window.location.search);
 // our covizu version number. may be different from upstream's version number.
-var covizuVersion = '1.0.0';
+var covizuVersion = urlParams.get('covizuVersion') || '';
 var dataUrl = urlParams.get('dataUrl') || '';
-var filesUrl = urlParams.get('filesUrl') || '';
-
-// get a list of clusters files from the current data version folder
-// in order to get the date of the latest file upload
-var clusters = [];
-var filesPath = window.location.hostname.includes('localhost')
-  ? 'cluster-list.json'
-  : '?format=json&prefix=' + covizuVersion + '/clusters.20';
-var filesUrlComplete = filesUrl + covizuVersion + '/' + filesPath;
-
-$.ajax({
-  async: false,
-  cache: false,
-  crossDomain: true,
-  dataType: "json",
-  headers: {
-    accept: "application/json",
-    "Access-Control-Allow-Origin": "*"
-  },
-  method: 'GET',
-  success: function (data) {
-    clusters = data;
-  },
-  url: filesUrlComplete
-});
-
-// get latest data date from the clusters filenames
-var clusterNames = [];
-// make sure filenames follow x.x.x/clusters.20xx-xx-xx.json pattern
-var clustersFilenameTest = /^(\d+\.){2}\d+\/(clusters\.)\d{4}(\-\d{2}){2}(\.json)$/;
-clusters.forEach(function (cluster) {
-  if (clustersFilenameTest.test(cluster.name)) {
-    clusterNames.push(cluster.name);
-  }
-})
-var clusterNamesSorted = clusterNames.concat().sort();
-var dataDate = clusterNamesSorted[clusterNamesSorted.length - 1].match(/\d{4}\-\d{2}\-\d{2}/)[0];
+var dataDate = urlParams.get('dataDate') || '';
 
 // setup custom options
 var dataUrlWithVersion = dataUrl + covizuVersion + '/';
@@ -58,7 +22,7 @@ $('#covizu-18n__en, #covizu-18n__fr').on('click', function (e) {
   e.preventDefault();
   var lang = e.target.id.split('__')[1];
   var filename = lang === 'en' ? 'index.html' : 'index-' + lang + '.html';
-  var newUrl = filename + '?dataUrl=' + dataUrl + '&filesUrl=' + filesUrl;
+  var newUrl = filename + '?dataUrl=' + dataUrl + '&covizuVersion=' + covizuVersion + '&dataDate=' + dataDate;
   location.href = newUrl;
 });
 
