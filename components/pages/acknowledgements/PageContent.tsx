f1e856fc94a5bb7ac6cd7d0e968680f15ee3b377
Anders Richardsson <2107110+caravinci@users.noreply.github.com>
2022-07-20 19:18:57 -0400

    update copyright year

--- components/pages/acknowledgements/PageContent.tsx
+++ components/pages/acknowledgements/PageContent.tsx
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright (c) 2021 The Ontario Institute for Cancer Research. All rights reserved
+ * Copyright (c) 2022 The Ontario Institute for Cancer Research. All rights reserved
  *
  *  This program and the accompanying materials are made available under the terms of
  *  the GNU Affero General Public License v3.0. You should have received a copy of the
@@ -31,7 +31,6 @@ import Contributors from './Contributors';
 
 import Individuals from './Individuals';
 import { format } from 'date-fns';
-
 
 const PageContent = (): ReactElement => {
   const theme: typeof defaultTheme = useTheme();
