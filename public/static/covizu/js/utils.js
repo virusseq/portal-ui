7936061c68e13cacfb22f9b3385b55daeff56348
Sam Rich <samrichca@users.noreply.github.com>
2022-08-19 16:08:02 -0400

    formatting

--- public/static/covizu/js/utils.js
+++ public/static/covizu/js/utils.js
@@ -4,12 +4,12 @@
  * Returns true if the string is an accession number
  */
 function isAccn(string) {
-  const accn_pat = /^EPI_ISL_[0-9]+$/i; // case-insensitive
+  const accn_pat = /^EPI_ISL_[0-9]+$/i;  // case-insensitive
   return accn_pat.test(string);
 }
 
 /**
- * Retruns true if the string is a lineage
+ * Retruns true if the string is a lineage 
  */
 function isLineage(string) {
   // const lin_pat = /[A-Z]\.[0-9]+/i;
@@ -23,12 +23,12 @@ function isLineage(string) {
  * @param {Date} date: The date to be formated
  */
 function formatDate(date) {
-  return d3.utcFormat('%Y-%m-%d')(date);
+  return d3.utcFormat("%Y-%m-%d")(date);
 }
 
 /**
  * Returns true if the date is in the correct format (YYYY-MM-DD)
- * @param {String} date
+ * @param {String} date 
  */
 function isDate(date) {
   const date_pat = /^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/;
@@ -36,11 +36,11 @@ function isDate(date) {
 }
 
 /**
- * Returns a date in UTC
+ * Returns a date in UTC 
  *
  * @param {String} date: The date to be converted
  */
 function utcDate(date) {
-  const dateObj = new Date(date);
-  return new Date(dateObj.getTime() + dateObj.getTimezoneOffset() * 60000);
+   const dateObj = new Date(date);
+   return new Date(dateObj.getTime() + dateObj.getTimezoneOffset() * 60000);
 }
