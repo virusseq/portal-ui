ce529eaa926935ab1504a0d808e06c836476eab2
Leonardo Rivera <leorivera_88@hotmail.com>
2024-12-11 16:29:25 -0500

    revert copyright date

--- components/pages/submission/Environmental/NewSubmissions/index.tsx
+++ components/pages/submission/Environmental/NewSubmissions/index.tsx
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright (c) 2024 The Ontario Institute for Cancer Research. All rights reserved
+ * Copyright (c) 2021 The Ontario Institute for Cancer Research. All rights reserved
  *
  *  This program and the accompanying materials are made available under the terms of
  *  the GNU Affero General Public License v3.0. You should have received a copy of the
@@ -56,8 +56,10 @@ const NewSubmissions = (): ReactElement => {
 			const formData = new FormData();
 
 			oneOrMoreCsv.forEach((csvFile) => {
-				// TODO: Fine grained permissions. Check if user has permission to upload Environmental data to this organization
-				// Taking the organization name from the filename
+				// TODO: Verify if the user's session permissions allow them
+				// to upload environmental data to this organization.
+
+				// Using the filename to determine the organization associated with the submission
 				const organizationName = csvFile.name.split('.')[0].toUpperCase();
 				formData.append('organization', organizationName);
 				// Submission service expects a file with the name of the schema it represents
