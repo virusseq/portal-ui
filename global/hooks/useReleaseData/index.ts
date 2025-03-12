2eff2ef3c3f91212f4eef43616a2c85fb32aa23e
Leonardo Rivera <leorivera_88@hotmail.com>
2025-03-12 13:30:00 -0400

    Update global/hooks/useReleaseData/index.ts
    Co-authored-by: Anders Richardsson <2107110+justincorrigible@users.noreply.github.com>

--- global/hooks/useReleaseData/index.ts
+++ global/hooks/useReleaseData/index.ts
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright (c) 2025 The Ontario Institute for Cancer Research. All rights reserved
+ * Copyright (c) 2021 The Ontario Institute for Cancer Research. All rights reserved
  *
  *  This program and the accompanying materials are made available under the terms of
  *  the GNU Affero General Public License v3.0. You should have received a copy of the
