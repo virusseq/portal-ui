9b764cda79529ac66f9b12a5f01ac19a04d05bb8
Dan DeMaria <ddemaria@oicr.on.ca>
2025-11-06 09:54:35 -0500

    Add new text

--- components/pages/acknowledgements/policies.tsx
+++ components/pages/acknowledgements/policies.tsx
@@ -29,12 +29,18 @@ export const iMicroSeqPolicy: PolicyProps = {
 	content: (
 		<>
 			<p>
-				You may use the data from the Canadian iMicroSeq Data Portal to author results obtained from your
-				analyses of relevant data, provided that your published results acknowledge the contribution of
-				iMicroSeq and its partners. We suggest using the following acknowledgement sentence:{' '}
+				The iMicroSeq Data Portal provides open access to microbial sequence, eDNA/eRNA/sequence amplicon data,
+				to support transparent, reproducible, and impactful scientific discovery. While all data are made openly
+				available, users are encouraged to contact the original data providers/owners if you are interested in
+				analyzing or publishing work involving their datasets. Collaboration can help ensure that data are
+				interpreted in context, and that credit and insights are shared.
+			</p>
+			<p>
+				If you use Canadian iMicroSeq Data Portal data in your work, we encourage acknowledgment of the
+				contribution of iMicroSeq and its partners. Pending publication, we suggest using an acknowledgement
+				sentence such as the following:{' '}
 				<strong>
-					"The results here are in whole, or in part based upon data hosted at the Canadian iMicroSeq Data
-					Portal:{' '}
+					"Data was provided by the Canadian iMicroSeq Data Portal:{' '}
 					<StyledLink
 						href="https://iMicroSeq-dataportal.ca/"
 						rel="noopener noreferrer"
@@ -42,9 +48,18 @@ export const iMicroSeqPolicy: PolicyProps = {
 					>
 						https://iMicroSeq-dataportal.ca/
 					</StyledLink>
-					. We wish to acknowledge Genome Canada, the Canadian Public Health Laboratory Network (CPHLN), and
-					iMicroSeq consortium participants for their contribution to the Portal."
+					with the support of Genome Canada and
+					<StyledLink
+						href="http://iMicroSeq.ca"
+						rel="noopener noreferrer"
+						target="_blank"
+					>
+						iMicroSeq.ca
+					</StyledLink>{' '}
+					consortium participants."
 				</strong>{' '}
+			</p>
+			<p>
 				You may redistribute the data available on this Data Portal under the same terms and conditions as
 				specified in this policy. You should not impose any additional or different terms or conditions on, or
 				apply any effective technological measures to the data, if doing so restricts the use of the data by
