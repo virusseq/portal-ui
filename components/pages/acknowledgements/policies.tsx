ba2048ea2242b3414ecfab5c433661197f57d4c3
Dan <ddemaria@oicr.on.ca>
2025-11-14 10:55:21 -0500

    🔖  Feat/ Acknowledgements pt2 (#495)
    * Add new text
    
    * Shortened iMicroSeqPolicy text
    
    * Add Scroll Margin css
    
    * Alphabetize Props
    
    * Fix spaces
    
    * change href to https
    
    ---------
    
    Co-authored-by: Anders Richardsson <2107110+justincorrigible@users.noreply.github.com>

--- components/pages/acknowledgements/policies.tsx
+++ components/pages/acknowledgements/policies.tsx
@@ -29,39 +29,35 @@ export const iMicroSeqPolicy: PolicyProps = {
 	content: (
 		<>
 			<p>
-				You may use the data from the Canadian iMicroSeq Data Portal to author results obtained from your
-				analyses of relevant data, provided that your published results acknowledge the contribution of
-				iMicroSeq and its partners. We suggest using the following acknowledgement sentence:{' '}
+				The iMicroSeq Data Portal provides open access to microbial sequence, eDNA/eRNA/sequence amplicon data,
+				to support transparent, reproducible, and impactful scientific discovery. While all data are made openly
+				available, users are encouraged to contact the original data providers/owners if you are interested in
+				analyzing or publishing work involving their datasets. Collaboration can help ensure that data are
+				interpreted in context, and that credit and insights are shared.
+			</p>
+			<p>
+				If you use Canadian iMicroSeq Data Portal data in your work, we encourage acknowledgment of the
+				contribution of iMicroSeq and its partners. Pending publication, we suggest using an acknowledgement
+				sentence such as the following:{' '}
 				<strong>
-					"The results here are in whole, or in part based upon data hosted at the Canadian iMicroSeq Data
-					Portal:{' '}
+					"Data was provided by the Canadian iMicroSeq Data Portal:{' '}
 					<StyledLink
 						href="https://iMicroSeq-dataportal.ca/"
 						rel="noopener noreferrer"
 						target="_blank"
 					>
 						https://iMicroSeq-dataportal.ca/
-					</StyledLink>
-					. We wish to acknowledge Genome Canada, the Canadian Public Health Laboratory Network (CPHLN), and
-					iMicroSeq consortium participants for their contribution to the Portal."
+					</StyledLink>{' '}
+					with the support of Genome Canada and{' '}
+					<StyledLink
+						href="https://iMicroSeq.ca"
+						rel="noopener noreferrer"
+						target="_blank"
+					>
+						iMicroSeq.ca
+					</StyledLink>{' '}
+					consortium participants."
 				</strong>{' '}
-				You may redistribute the data available on this Data Portal under the same terms and conditions as
-				specified in this policy. You should not impose any additional or different terms or conditions on, or
-				apply any effective technological measures to the data, if doing so restricts the use of the data by
-				others.
-			</p>
-
-			<p>
-				If you wish to publish research using these data, you are encouraged to contact us at{' '}
-				<EmailLink email="imicroseq-dataportal@lists.sfu.ca">imicroseq-dataportal@lists.sfu.ca</EmailLink>{' '}
-				before analyzing the data to ensure that those who have generated the data may be involved in its
-				analysis. You are responsible for making the best efforts to collaborate with representatives of the
-				data providers responsible for obtaining the specimens and to involve them in your analyses and
-				research. The metadata available on the Canadian VirusSeq Data Portal comprises a subset of the Canadian
-				COVID-19 related datasets. You may potentially have access to more data through formal collaborations
-				with the CPHLN and CanCOGeN VirusSeq members. You are encouraged to contact us at{' '}
-				<EmailLink email="imicroseq-dataportal@lists.sfu.ca">imicroseq-dataportal@lists.sfu.ca</EmailLink> to
-				obtain additional information for this purpose.
 			</p>
 		</>
 	),
